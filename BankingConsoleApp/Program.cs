8c6c073781ef69c1b68932ac03a9124bc1b6fa64
ttwin@nete2asia.com <ttwin@nete2asia.com>
2022-08-22 20:52:15 +0630

    Add choice to exit

--- BankingConsoleApp/Program.cs
+++ BankingConsoleApp/Program.cs
@@ -4,58 +4,64 @@ using BankingConsoleApp.Models;
 
 Console.WriteLine("Welcome To CLI Banking Application");
 Console.WriteLine("----------------------");
-
-Console.WriteLine("What do you want to? Key in 1 to Deposite, 2 to Withdraw, 3 to Transfer:");
-var choice = int.Parse(Console.ReadLine());
-if (choice == 1)
+while (true)
 {
-    Console.WriteLine("Account No?:");
-    var accNo = Console.ReadLine();
+    Console.WriteLine("What do you want to? Key in 1 to Deposite, 2 to Withdraw, 3 to Transfer, 0 to Exit:");
+    var choice = int.Parse(Console.ReadLine());
+    if (choice == 1)
+    {
+        Console.WriteLine("Account No?:");
+        var accNo = Console.ReadLine();
 
-    Console.WriteLine("Amount ?:");
-    var amount = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Amount ?:");
+        var amount = decimal.Parse(Console.ReadLine());
 
-    var acc = new Account(accNo, 100000);
-    acc.Deposite(amount);
+        var acc = new Account(accNo, 100000);
+        acc.Deposite(amount);
 
-    Console.WriteLine($"After Deposite => Acc No:{acc.AccNo}, Balance:{acc.Balance}");
+        Console.WriteLine($"After Deposite => Acc No:{acc.AccNo}, Balance:{acc.Balance}");
 
-}
-if (choice == 2)
-{
-    Console.WriteLine("Account No?:");
-    var accNo = Console.ReadLine();
+    }
+    if (choice == 2)
+    {
+        Console.WriteLine("Account No?:");
+        var accNo = Console.ReadLine();
 
-    Console.WriteLine("Amount ?:");
-    var amount = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Amount ?:");
+        var amount = decimal.Parse(Console.ReadLine());
 
-    var acc = new Account(accNo, 100000);
-    acc.Withdraw(amount);
+        var acc = new Account(accNo, 100000);
+        acc.Withdraw(amount);
 
-    Console.WriteLine($"After Withdraw => Acc No:{acc.AccNo}, Balance:{acc.Balance}");
+        Console.WriteLine($"After Withdraw => Acc No:{acc.AccNo}, Balance:{acc.Balance}");
 
-}
-else if (choice == 3)
-{
-    Console.WriteLine("From Account No?:");
-    var fromAccNo = Console.ReadLine();
+    }
+    else if (choice == 3)
+    {
+        Console.WriteLine("From Account No?:");
+        var fromAccNo = Console.ReadLine();
 
-    Console.WriteLine("To Account No?:");
-    var toAccNo = Console.ReadLine();
+        Console.WriteLine("To Account No?:");
+        var toAccNo = Console.ReadLine();
 
-    Console.WriteLine("Amount ?:");
-    var amount = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Amount ?:");
+        var amount = decimal.Parse(Console.ReadLine());
 
-    var fromAcc = new Account(fromAccNo, 100000);
-    var toAcc = new Account(toAccNo, 100000);
-    AccountTransferService.Transfer(fromAcc, toAcc, amount);
+        var fromAcc = new Account(fromAccNo, 100000);
+        var toAcc = new Account(toAccNo, 100000);
+        AccountTransferService.Transfer(fromAcc, toAcc, amount);
 
-    Console.WriteLine($"After Transfer => Acc No:{fromAcc.AccNo}, Balance:{fromAcc.Balance}");
-    Console.WriteLine($"After Transfer => Acc No:{toAcc.AccNo}, Balance:{toAcc.Balance}");
+        Console.WriteLine($"After Transfer => Acc No:{fromAcc.AccNo}, Balance:{fromAcc.Balance}");
+        Console.WriteLine($"After Transfer => Acc No:{toAcc.AccNo}, Balance:{toAcc.Balance}");
 
-}
-else {
-    Console.WriteLine("Invalid choice!");
+    }
+    else if (choice == 0) {
+        return;
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice!");
+    }
 }
 
 //var accA = new Account("Account-A", 100000);
