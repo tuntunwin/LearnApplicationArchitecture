fd90c169f3f1c6ca52e98547cb3e82179c00c187
MieMie <chowai206@gmail.com>
2022-08-29 21:34:02 +0630

    add Event, Odd and RoundUp extensions.

---
+++ BankingConsoleApp/MyExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingConsoleApp
+{
+    static class MyExtensions
+    {
+        public static Boolean IsEven(this int i)
+        {
+            if (i % 2 == 0) { return true; }
+            else { return false; }
+        }
+
+        public static Boolean IsOdd(this int i)
+        {
+            if (i % 2 > 0) { return true; }
+            else { return false; }
+        }
+
+        public static decimal RoundUp(this decimal dec)
+        {
+           return Math.Round(dec);
+        }
+    }
+}
